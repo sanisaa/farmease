e32573c68731223bfaab84ace624b21bb36b883f
Sanisha Maharjan <71261641+sanisaa@users.noreply.github.com>
2023-07-03 23:47:14 +0545

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
-# farmease
+# FarmEase
+
+- FarmEase  is a machine learning-based project designed to provide predictions and recommendations for farmers. The system uses different algorithms to predict crops, recommend fertilizers, and provide rainfall and yield predictions to help farmers make informed decisions about their crops.
+- IT also has direct crop sales to customer with real payment interface using Stripe API.
+- Other supporting features are Chatbot using OPENAI's gpt-3.5-turbo model, Weather Forecast upto 4 days using Weather API, Agriculture realetd news using News API.
+
+## Pre Requisites
+
+## Features
+- Crop Prediction
+- Crop Recommendation
+- Fertilizer Recommendation
+- Rainfall Prediction
+- OTP Verification through mail
+- Agriculture realetd news using News API
+- Dynamically changing quotes using OpenAI's API
+- Weather Forecast upto 4 days using OpenWeatherMap API
+
+
+
+## Technologies Used
+- Python
+- PHP
+- Pandas
+- NumPy
+- JavaScript
+- HTML/CSS
+- Bootstrap4
+- Scikit-learn
+
+## Dataset
+### Crop Recommendation Dataset
+- N
+- P
+- K
+- Temperature
+- Humidity
+- pH
+- Rainfall
+- Label
+
+### Fertilizer Recommendation Dataset
+- Temparature
+- Humidity
+- Soil Moisture
+- Soil Type
+- Crop Type
+- Nitrogen
+- Phosphorous
+- Potassium
+- Fertilizer Name
+
+### Rainfall Prediction Dataset
+- DISTRICT
+- YEAR
+- JAN
+- FEB
+- MAR
+- APR
+- MAY
+- JUN
+- JUL
+- AUG
+- SEP
+- OCT
+- NOV
+- DEC
+- ANNUAL
+
---
+++ index.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/png" href="assets/img/fm-logo.png" />
+    <title>FarmEase</title>
+
+  <!--     Fonts and icons     -->
+  
+	<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+	integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" 
+	integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+	
+	<link rel="stylesheet" href=" https://cdnjs.cloudflare.com/ajax/libs/bootstrap-social/5.1.1/bootstrap-social.min.css "/>
+	
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+  <link href="https://use.fontawesome.com/releases/v5.0.6/css/all.css" rel="stylesheet">
+ 
+  <!-- Nucleo Icons-->
+  <link href="assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="assets/css/nucleo-svg.css" rel="stylesheet" />
+
+  <link rel="stylesheet" href="assets/css/theme.css" type="text/css"> 
+
+</head>
+
+  <body class="bg-white" id="top">
+  <main class="main" id="top">
+    <!-- Navbar -->
+    <nav
+      id="navbar-main"
+      class="navbar navbar-expand-lg navbar-light fixed-top py-3 bg-light opacity-85" data-navbar-on-scroll="data-navbar-on-scroll">
+      <div class="container"><a class="navbar-brand" href="index.php"><img class="d-inline-block align-top img-fluid" src="assets/img/fm-logo.png" alt="" width="100" /><span class="text-theme font-monospace fs-4 ps-2"></span></a>
+      <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+         <div class="navbar-collapse collapse border-top border-lg-0 mt-4 mt-lg-0" id="navbarSupportedContent">
+         <ul class="navbar-nav align-items-lg-center ml-auto topnav" id="nav">
+		  <li class="nav-item px-2"><a class="nav-link fw-medium active" aria-current="page" href="#header">Home</a></li>
+		  <li class="nav-item px-2"><a class="nav-link fw-medium" aria-current="page" href="contact.php">Contact</a></li>
+          <li class="nav-item px-2"><a class="nav-link fw-medium" href="#Opportuanities">Opportunities</a></li>
+          <li class="nav-item px-2"><div class="dropdown show">
+		  <a class="nav-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		                  <span class="nav-link-inner--text"
+                  ><i class="fas fa-user-plus"></i> Sign Up</span
+                >
+		  </a>
+
+		  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+			<a class="dropdown-item" href="farmer/fregister.php">Farmer</a>
+			<a class="dropdown-item" href="customer/cregister.php">Customer</a>
+		  </div>
+		</div>
+			</li>
+			
+		  
+				  <li class="nav-item px-2">
+			   <div class="dropdown show ">
+		  <a class="nav-link dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+		                  <span class="nav-link-inner--text"
+                  ><i class="fas fa-sign-in-alt"></i> Login</span
+                >
+		  </a>
+
+		  <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+			<a class="dropdown-item" href="farmer/flogin.php">Farmer</a>
+			<a class="dropdown-item" href="customer/clogin.php">Customer</a>
+			<a class="dropdown-item" href="admin/alogin.php">Admin </a>
+		  </div>
+		</div>
+			</li>
+			
+          
+          </ul>
+          
+        </div>
+      </div>
+    </nav>
+    <!-- End Navbar -->
+    <section class="py-0" id="header">
+        <div class="bg-holder d-none d-md-block" style="background-image:url(assets/img/illustrations/2.png);background-position:right top;background-size:contain;">
+        </div>
+
+        <div class="container">
+          <div class="row align-items-center min-vh-75 min-vh-lg-100">
+            <div class="col-md-7 col-lg-6 col-xxl-5 py-6 text-sm-start text-center">
+              <h1 class="mt-6 mb-sm-4 fw-semi-bold lh-sm fs-4 fs-lg-5 fs-xl-6">Bridging the Gap for<br class="d-block d-lg-block" />Farmers & Customers</h1>
+              <p class="mb-4 fs-1">Welcome to Farmease, your comprehensive agriculture portal offering a range of services to revolutionize farming practices. We specialize in leveraging advanced technologies to enhance productivity, mitigate risks, and optimize agricultural decision-making.</p>
+              <!-- <a class="btn btn-lg btn-success" href="#" role="button">Learn More</a> -->
+            </div>
+          </div>
+        </div>
+      </section>
+<!-- <div class="wrapper" > -->
+
+<!-- Page Content -->
+
+    <div class="section features-6 text-dark bg-white" id="services">
+      <div class="container ">
+
+        <div class="row">
+                <div class="col-md-8 mx-auto text-center">
+                    <h3 class="display-3 ">Features</h3>
+                </div>
+            </div>
+			<br>
+        <div class="row align-items-center">
+		
+          <div class="col-lg-6">
+            <div class="info info-horizontal info-hover-success">
+              <div class="description pl-4">
+                <h3 class="title" >Farmers</h3>
+           <p class=" ">
+           Farmers can benefit from personalized crop and fertilizer recommendations, accurate weather predictions, access to agricultural news, and the opportunity to directly sell their crops to customers. These features empower farmers to make informed decisions, optimize their yield, and maximize profitability, while establishing transparent and fair trade relationships.
+           </p>
+                        
+              </div>
+            </div>
+
+          </div>
+		  
+		  
+          <div class="col-lg-6 col-10 mx-md-auto d-none d-md-block">
+            <img class="ml-lg-5  pull-right" src="assets/img/farmer.png" width="100%">
+          </div>
+        </div>
+		
+		
+		<div class="row align-items-center">
+				  <div class="col-lg-6 col-10 mx-md-auto d-none d-md-block">
+            <img class="ml-lg-5" src="assets/img/customer.png" width="80%">
+          </div>
+     
+		
+		
+          <div class="col-lg-6">
+			
+            <div class="info info-horizontal info-hover-warning mt-5">
+              <div class="description pl-4">
+                <h3 class="title">Customers</h3>
+                <p class=" ">Customers enjoy the convenience of purchasing fresh and locally sourced produce directly from farmers. They can trace the origin of their crops, discover and connect with local farmers, and experience a transparent farm-to-table journey. Online purchasing features provide a seamless and secure way to browse products, place orders, and support local farmers.</p>
+              </div>
+            </div>
+      
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <section class="py-5" id="Opportuanities">
+        
+       
+
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-9 mx-auto text-center mb-3">
+              <h3 class="display-3">Opportunities</h3>
+              <p class="mb-5">Revolutionize agriculture with Farmease - connecting farmers and customers like never before.</p>
+            </div>
+          </div>
+          <div class="row flex-center h-100">
+            <div class="col-xl-9">
+              <div class="row">
+                <div class="col-md-4 mb-5">
+                  <div class="card h-100 shadow px-4 px-md-2 px-lg-3 card-span pt-6">
+                    <div class="text-center text-md-start card-hover"><img class="ps-3 icons" src="assets/img/icons/farmer.svg" height="60" alt="" />
+                      <div class="card-body">
+                        <h6 class="fw-bold fs-1 heading-color">Connect directly with farmers</h6>
+                        <p class="mt-3 mb-md-0 mb-lg-2">Discover the power of direct connection by engaging with our farmers. Experience the satisfaction of supporting local agriculture and building a stronger farming community.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-4 mb-5">
+                  <div class="card h-100 shadow px-4 px-md-2 px-lg-3 card-span pt-6">
+                    <div class="text-center text-md-start card-hover"><img class="ps-3 icons" src="assets/img/icons/growth.svg" height="60" alt="" />
+                      <div class="card-body">
+                        <h6 class="fw-bold fs-1 heading-color">Unlocking Potential: Explore the Possibilities</h6>
+                        <p class="mt-3 mb-md-0 mb-lg-2">Discover a world of possibilities as farmers access personalized recommendations, accurate weather predictions, and valuable agricultural news, while customers enjoy the convenience of purchasing fresh produce directly from farmers.</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-4 mb-5">
+                  <div class="card h-100 shadow px-4 px-md-2 px-lg-3 card-span pt-6">
+                    <div class="text-center text-md-start card-hover"><img class="ps-3 icons" src="assets/img/icons/future.svg" height="60" alt="" />
+                      <div class="card-body">
+                        <h6 class="fw-bold fs-1 heading-color">Embrace the Future: Tap into Promising Prospects</h6>
+                        <p class="mt-3 mb-md-0 mb-lg-2">Embrace the future by tapping into promising prospects as farmers optimize yield, embrace sustainable practices, and connect with new markets, while customers access high-quality, locally sourced produce and support sustainable farming practices.
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+     
+	<!-- ======================================================================================================================================== -->
+
+      
+      </main>
+<?php require("footer.php");?>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+    <link href="https://fonts.googleapis.com/css2?family=Chivo:wght@300;400;700;900&amp;display=swap" rel="stylesheet">
+
+</body>
+
+</html>
